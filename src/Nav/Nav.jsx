721753b2561be502fcf9130ae9969aa6be1ffb89
FrogMan322 <gojkov.nemanja69@gmail.com>
2024-01-29 18:38:12 +0100

    Adding progres line

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -20,57 +20,59 @@ function Nav() {
     dispatch(storeItemsActions.slideCart(value));
   }
   return (
-    <motion.div
-      initial={{ y: "-100%" }}
-      animate={{ y: 0 }}
-      transition={{ type: "Tween" }}
-      className={classes.container}
-    >
-      <h1 className={classes["store__name"]}>
-        <NavLink
-          to={"/"}
-          className={({ isActive }) => {
-            return isActive ? classes.isVisiting : undefined;
-          }}
-        >
-          Random Store
-        </NavLink>
-      </h1>
-
-      <h1 className={classes["store__name"]}>
-        <NavLink
-          to={"/checkout"}
-          className={({ isActive }) => {
-            return isActive ? classes.isVisiting : undefined;
-          }}
-        >
-          Checkout
-        </NavLink>
-      </h1>
-
+    <>
       <motion.div
-        whileHover={{
-          scale: 1.4,
-          rotate: -10,
-        }}
-        transition={{ type: "spring", duration: 0.4 }}
-        onClick={() => {
-          setCartValue(true);
-        }}
-        className={classes["cart"]}
+        initial={{ y: "-100%" }}
+        animate={{ y: 0 }}
+        transition={{ type: "Tween" }}
+        className={classes.container}
       >
-        <i className="bi bi-cart"></i>
-        <motion.p
-          key={totalQuantity}
-          initial={{ scale: 1 }}
-          animate={{ scale: [1, 1.3, 1] }}
-          transition={{ duration: 0.2 }}
-          className={classes["cart__number"]}
+        <h1 className={classes["store__name"]}>
+          <NavLink
+            to={"/"}
+            className={({ isActive }) => {
+              return isActive ? classes.isVisiting : undefined;
+            }}
+          >
+            Random Store
+          </NavLink>
+        </h1>
+
+        <h1 className={classes["store__name"]}>
+          <NavLink
+            to={"/checkout"}
+            className={({ isActive }) => {
+              return isActive ? classes.isVisiting : undefined;
+            }}
+          >
+            Checkout
+          </NavLink>
+        </h1>
+
+        <motion.div
+          whileHover={{
+            scale: 1.4,
+            rotate: -10,
+          }}
+          transition={{ type: "spring", duration: 0.4 }}
+          onClick={() => {
+            setCartValue(true);
+          }}
+          className={classes["cart"]}
         >
-          {totalQuantity}
-        </motion.p>
+          <i className="bi bi-cart"></i>
+          <motion.p
+            key={totalQuantity}
+            initial={{ scale: 1 }}
+            animate={{ scale: [1, 1.3, 1] }}
+            transition={{ duration: 0.2 }}
+            className={classes["cart__number"]}
+          >
+            {totalQuantity}
+          </motion.p>
+        </motion.div>
       </motion.div>
-    </motion.div>
+    </>
   );
 }
 
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,9 +1,16 @@
 import Nav from "../Nav/Nav";
 import { Outlet } from "react-router-dom";
 import Modal from "../Cart/Modal";
+import { motion, useScroll } from "framer-motion";
+import classes from "./Root.module.css";
 function RootLayout() {
+  const { scrollYProgress } = useScroll();
   return (
     <>
+      <motion.div
+        className={classes.line}
+        style={{ scaleX: scrollYProgress }}
+      />
       <Nav />
       <Modal />
       <Outlet />
