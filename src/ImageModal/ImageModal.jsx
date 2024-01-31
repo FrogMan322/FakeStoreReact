07d87ddad0a6fd0b8849b8ea7a01987fefd20fa9
FrogMan322 <gojkov.nemanja69@gmail.com>
2024-01-31 13:29:51 +0100

    Updating modal animation

--- src/ImageModal/ImageModal.jsx
+++ src/ImageModal/ImageModal.jsx
@@ -35,7 +35,7 @@ function ModalImage() {
     <motion.div
       initial={{ opacity: 0, y: -100 }}
       animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: 100 }}
+      exit={{ opacity: 0, y: -100 }}
       transition={{ duration: 0.4 }}
       className={classes.imageContainer}
     >
